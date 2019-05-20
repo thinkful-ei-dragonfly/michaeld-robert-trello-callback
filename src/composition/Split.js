614358b94bd89694a394111ac1bca2f48a610da1
Kasia <kasia@miroartdesign.com>
2019-05-20 15:55:20 -0400

    basics

---
+++ src/composition/Split.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import './Split.css';
+
+
+function Split(props) {
+    const combinedClassName = `split ${props.className}`
+    return (
+        <div className={combinedClassName}>{props.children}</div>
+    );
+}
+export default Split;
+
