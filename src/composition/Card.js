add45719805b00969539a86db6a1ae6b372b863a
Michael Dusaniwskyj <dusaniwskyj@gmail.com>
2019-05-20 20:20:38 -0400

    added styles and delete button. add +add random card next

--- src/composition/Card.js
+++ src/composition/Card.js
@@ -4,8 +4,10 @@ function Card(props) {
 
   return (
     <div className='Card'>
+      <button type='delete'>delete</button>
       <h3>{props.title}</h3>
       <p>{props.content}</p>
+      
     </div>
   )
 }
